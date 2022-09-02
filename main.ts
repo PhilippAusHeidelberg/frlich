1af9be7514e77b0ba795aa2b27386c285c959081
PhilippAusHeidelberg <110686148+PhilippAusHeidelberg@users.noreply.github.com>
2022-09-02 18:50:25 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,34 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showIcon(IconNames.StickFigure)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showIcon(IconNames.StickFigure)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showIcon(IconNames.StickFigure)
+    basic.showLeds(`
+        # . # . #
+        . # # # .
+        . . # . .
+        . # . # .
+        . # . # .
+        `)
+    basic.showIcon(IconNames.StickFigure)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+})
